b5caf5f8f0985506a690cbad03e817935ea12f28
EC2 Default User <ec2-user@ip-10-0-1-75.us-east-2.compute.internal>
2020-06-29 04:50:37 +0000

    playbooks

---
+++ sql-statements/create.sh
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    name varchar(128) PRIMARY KEY,
+    password varchar(128),
+    full_name varchar(128) 
+)
+
+INSERT into USERS (name, password, full_name) VALUES ('Matt', 'Wrong', 'Just Testing')
